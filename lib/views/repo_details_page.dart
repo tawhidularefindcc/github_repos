4aac287393810e0bbff274b4e4ced4be555ba22d
Tawhidul Arefin <tawhidularefin530@gmail.com>
2025-11-06 13:53:33 +0600

    Modified: repo Details Page

--- lib/views/repo_details_page.dart
+++ lib/views/repo_details_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+import 'package:url_launcher/url_launcher.dart';
 import '../models/repo_model.dart';
 
 class RepoDetailPage extends StatelessWidget {
@@ -9,6 +11,15 @@ class RepoDetailPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final args = Get.arguments as Map<String, dynamic>?;
     final RepoModel repo = args!['repo'] as RepoModel;
+
+    String formatDate(String date) {
+      try {
+        final parsed = DateTime.parse(date);
+        return DateFormat('MMM dd, yyyy').format(parsed);
+      } catch (_) {
+        return date;
+      }
+    }
 
     return Scaffold(
       appBar: AppBar(title: Text(repo.name)),
@@ -32,16 +43,16 @@ class RepoDetailPage extends StatelessWidget {
               ],
             ),
             const SizedBox(height: 12),
-            Text('Updated: ${repo.updatedAt.toLocal()}'),
+            Text('Updated: ${formatDate(repo.updatedAt.toLocal().toString())}'),
             const SizedBox(height: 12),
             ElevatedButton(
               onPressed: () async {
-                // open in browser
-                // Use url_launcher package if desired; for now we show a snackbar
-                Get.snackbar(
-                  'Open',
-                  'Open repository in browser: \${repo.htmlUrl}',
-                );
+                final uri = Uri.parse(repo.htmlUrl);
+                if (await canLaunchUrl(uri)) {
+                  await launchUrl(uri, mode: LaunchMode.externalApplication);
+                } else {
+                  Get.snackbar('Error', 'Could not launch ${repo.htmlUrl}');
+                }
               },
               child: const Text('Open on GitHub'),
             ),
